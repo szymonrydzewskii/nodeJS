ef255120c1184e5d68f2b92f014ac6f451c76fad
Szymon <s.rydzewski26@gmail.com>
2024-11-02 12:43:41 +0100

    poprawa interfejsu bo zle napisalem, tworzenie nowego obiektu javascriptowego i test czy dobrze sie tworzy

--- json-data-manager/data-manager.js
+++ json-data-manager/data-manager.js
@@ -1,14 +1,26 @@
 const fs = require("fs")
 const readline = require("readline")
 const rl = readline.createInterface({
-    process: stdin,
-    process: stdout
+    input: process.stdin,
+    output: process.stdout
 })
 
 rl.question("Podaj nazwe pliku z ktorym chcesz pracowac: ", (sciezka) => {
     rl.question("Dodanie nowego obiektu - 1, Wyswietlenie danych - 2: ", (wybor) => {
         if(wybor === "1"){
-
+            rl.question("Podaj imie: ", (name) => {
+                rl.question("Podaj wiek: ", (age) => {
+                    rl.question("Podaj maila: ", (email) =>{
+                        const daneWpisane = {
+                            name: name,
+                            age: age,
+                            email: email
+                        }
+                        console.log(daneWpisane)
+                        rl.close()
+                    })
+                })
+            })
         }
         else if(wybor === "2"){
 
