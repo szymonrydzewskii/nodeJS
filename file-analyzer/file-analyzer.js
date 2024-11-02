5da2fbe144742c8a8fa32ee36df057a32b1cf89e
Szymon <s.rydzewski26@gmail.com>
2024-11-02 14:32:29 +0100

    Stworzenie nowego emittera, emitowanie rozpoczecia i zakonczenia analizy, zadanie pytania uzytkownikowi o sciezke katalogu

--- file-analyzer/file-analyzer.js
+++ file-analyzer/file-analyzer.js
@@ -1,8 +1,18 @@
 const fs = require("fs")
 const path = require("path")
 const EventEmitter = require("events")
+
 const readline = require("readline")
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
+})
+
+const emitter = new EventEmitter()
+
+emitter.on('plikStart', (plik) => console.log(`Rozpoczęto analizę pliku: ${plik}`))
+emitter.on('plikKoniec', (plik) => console.log(`Zakończono analizę pliku: ${plik}`))
+
+rl.question("Podaj ścieżkę do katalogu: ", (sciezka) => {
+
 })
