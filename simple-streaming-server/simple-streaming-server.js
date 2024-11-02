1ec68917d704451783bb14baa280e367307f7738
Szymon <s.rydzewski26@gmail.com>
2024-11-02 15:13:25 +0100

    Stworzenie serwera i nasluchiwanie go na porcie 3000

---
+++ simple-streaming-server/simple-streaming-server.js
@@ -0,0 +1,12 @@
+const fs = require("fs")
+const http = require("http")
+const path = require('path')
+
+const server = http.createServer((req, res) => {
+    const url = new URL(req.url, 'http://localhost:3000')
+    const filename = url.searchParams.get('file');
+
+server.listen(3000, ()=>{
+    console.log('Serwer działa na porcie 3000')
+    })
+})
