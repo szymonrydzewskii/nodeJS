135309578c08abd9e45b5be577aecfb28038cfae
Szymon <s.rydzewski26@gmail.com>
2024-11-02 15:31:51 +0100

    Dodano obsluge jezeli pliku nie ma albo uzytkownik nie wpisal jeszcze nazwy pliku do linku

--- simple-streaming-server/simple-streaming-server.js
+++ simple-streaming-server/simple-streaming-server.js
@@ -4,8 +4,24 @@ const path = require('path')
 
 const server = http.createServer((req, res) => {
     const url = new URL(req.url, 'http://localhost:3000')
-    const filename = url.searchParams.get('file');
-})
+    const filename = url.searchParams.get('file')
+
+    if(!filename){
+        res.writeHead(400, {'content-type': 'text/plain'})
+        res.end("Podaj nazwe pliku w linku np. (http://localhost:3000?file=plikWatcher.txt)")
+        return
+    }
+    const sciezka = path.join('./file-watcher', filename)
+
+    const stream = fs.createReadStream(sciezka)
+
+    stream.on('error', ()=>{
+        res.writeHead(404, {'content-type': 'text/plain'})
+        res.end("Plik nie istnieje")
+    })
+    stream.pipe(res)
+    });
+
 server.listen(3000, ()=>{
     console.log('Serwer działa na porcie 3000')
     })
