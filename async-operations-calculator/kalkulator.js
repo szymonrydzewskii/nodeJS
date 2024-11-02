94c45a102c27f3913794cb4b5ae72fedad2233ff
Szymon <s.rydzewski26@gmail.com>
2024-11-02 12:11:10 +0100

    Obsluga bledow, sprawdzanie czy liczba to napewno liczba

--- async-operations-calculator/kalkulator.js
+++ async-operations-calculator/kalkulator.js
@@ -5,6 +5,13 @@ const rl = readline.createInterface({
 })
 
 function metoda_callback(a, b, operacja){
+        setTimeout(()=> {
+        if (isNaN(a) || isNaN(b)) {
+            console.error("podano nieprawidłową liczbę")
+            rl.close()
+            return
+        }
+
         var wynik = 0
         if(operacja === "1"){
             wynik = a + b
@@ -12,10 +19,11 @@ function metoda_callback(a, b, operacja){
         else if(operacja === "2"){
             wynik = a * b
         } else{
-            return console.log("błąd"), rl.close()
+            return console.error("błąd"), rl.close()
         }
         console.log(wynik)
         rl.close()
+    }, 1000)
 }
 
 rl.question("podaj pierwszą liczbę: ", (input_a) => {
@@ -23,7 +31,14 @@ rl.question("podaj pierwszą liczbę: ", (input_a) => {
         const a = parseInt(input_a)
         const b = parseInt(input_b)
         rl.question("Dodawanie - 1, Mnożenie - 2: ", (operacja) =>{
-            metoda_callback(a, b, operacja)
+        
+            metoda_callback(a, b, operacja, (err, wynik) =>{
+                if(err){
+                    console.error("błąd")
+                    return
+                }
+                console.log(wynik)
+            })
         })
     })
 })
