128f559bb53cc2126aaa9545bce48ff170870a2d
Szymon <s.rydzewski26@gmail.com>
2024-11-02 13:24:36 +0100

    Stworzenie wyswietlania danych z pliku oraz wypisywanie bledu jezeli nie ma takiego pliku

--- json-data-manager/data-manager.js
+++ json-data-manager/data-manager.js
@@ -25,15 +25,24 @@ rl.question("Podaj nazwe pliku z ktorym chcesz pracowac: ", (sciezka) => {
                         fs.writeFileSync(`json-data-manager/${sciezka}.json`, JSON.stringify(tablica, null, 2))
                         }
                         catch(err){
-                            fs.writeFileSync(`json-data-manager/${sciezka}.json`, JSON.stringify([daneWpisane]))
+                            fs.writeFileSync(`json-data-manager/${sciezka}.json`, JSON.stringify(daneWpisane))
                         }
+                        console.log("Zapisano dane")
                         rl.close()
                     })
                 })
             })
         }
         else if(wybor === "2"){
-
+            try{
+            const data = fs.readFileSync(`json-data-manager/${sciezka}.json`, 'utf8')
+            console.log(JSON.parse(data))
+            }
+            catch{
+                console.error("Nie ma takiego pliku")
+            }finally{
+                rl.close()
+            }
         }else{
             console.error("błąd")
         }
