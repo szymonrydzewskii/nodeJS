95c726de6e81cf28748533094d61d5ece898c4c5
Szymon <s.rydzewski26@gmail.com>
2024-11-02 12:38:05 +0100

    Pytania co chce uzytkownik zrobic i obsluga wyboru

--- json-data-manager/data-manager.js
+++ json-data-manager/data-manager.js
@@ -4,3 +4,16 @@ const rl = readline.createInterface({
     process: stdin,
     process: stdout
 })
+
+rl.question("Podaj nazwe pliku z ktorym chcesz pracowac: ", (sciezka) => {
+    rl.question("Dodanie nowego obiektu - 1, Wyswietlenie danych - 2: ", (wybor) => {
+        if(wybor === "1"){
+
+        }
+        else if(wybor === "2"){
+
+        }else{
+            console.error("błąd")
+        }
+    })
+})
