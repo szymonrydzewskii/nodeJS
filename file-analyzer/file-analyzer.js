dc29437de070a8842e491bd9ec73f3c7bdb35ed1
Szymon <s.rydzewski26@gmail.com>
2024-11-02 14:27:21 +0100

    dodanie readline i stworzenie interfejsu

--- file-analyzer/file-analyzer.js
+++ file-analyzer/file-analyzer.js
@@ -1,3 +1,8 @@
 const fs = require("fs")
 const path = require("path")
 const EventEmitter = require("events")
+const readline = require("readline")
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
