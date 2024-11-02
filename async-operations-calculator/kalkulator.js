19f55e845b0b28d7545548c85a51325bd9da1f9c
Szymon <s.rydzewski26@gmail.com>
2024-11-02 11:54:14 +0100

    Przetestowanie czy program poprawnie pobiera liczby

--- async-operations-calculator/kalkulator.js
+++ async-operations-calculator/kalkulator.js
@@ -8,5 +8,8 @@ rl.question("podaj pierwszą liczbę: ", (input_a) => {
     rl.question("podaj drugą liczbę: ", (input_b) => {
         const a = parseInt(input_a)
         const b = parseInt(input_b)
+
+        console.log(a + b)
+        rl.close()
     })
 })
