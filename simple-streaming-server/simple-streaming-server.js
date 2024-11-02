43ab1e805573b0ff1cadbe8d32e7126901954a71
Szymon <s.rydzewski26@gmail.com>
2024-11-02 15:15:19 +0100

    poprawa bledu

--- simple-streaming-server/simple-streaming-server.js
+++ simple-streaming-server/simple-streaming-server.js
@@ -5,8 +5,7 @@ const path = require('path')
 const server = http.createServer((req, res) => {
     const url = new URL(req.url, 'http://localhost:3000')
     const filename = url.searchParams.get('file');
-
+})
 server.listen(3000, ()=>{
     console.log('Serwer działa na porcie 3000')
     })
-})
