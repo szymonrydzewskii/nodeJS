1e1c6c098ef4baaaf8aa3f30f0a5fe239ffd0cba
Szymon <s.rydzewski26@gmail.com>
2024-11-02 11:22:19 +0100

    Stworzenie zadawania pytan

--- data-logger/data-logger.js
+++ data-logger/data-logger.js
@@ -4,3 +4,11 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
+
+rl.question("Podaj swoje imie: ", (name) => {
+    rl.question("Podaj swoje nazwisko: ", (surname) => {
+        rl.question("Podaj swój wiek: ", (age) => {
+            
+        })
+    })
+})
