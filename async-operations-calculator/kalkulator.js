e08a71f8d52d64f6165e6cbb3823eeebe5aa131e
Szymon <s.rydzewski26@gmail.com>
2024-11-02 12:01:09 +0100

    Stworzenie funkcji metoda_callback oraz przetestowanie jej dzialania

--- async-operations-calculator/kalkulator.js
+++ async-operations-calculator/kalkulator.js
@@ -4,12 +4,26 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
+function metoda_callback(a, b, operacja){
+        var wynik = 0
+        if(operacja === "1"){
+            wynik = a + b
+        }
+        else if(operacja === "2"){
+            wynik = a * b
+        } else{
+            return console.log("błąd"), rl.close()
+        }
+        console.log(wynik)
+        rl.close()
+}
+
 rl.question("podaj pierwszą liczbę: ", (input_a) => {
     rl.question("podaj drugą liczbę: ", (input_b) => {
         const a = parseInt(input_a)
         const b = parseInt(input_b)
-
-        console.log(a + b)
-        rl.close()
+        rl.question("Dodawanie - 1, Mnożenie - 2: ", (operacja) =>{
+            metoda_callback(a, b, operacja)
+        })
     })
 })
