e3e41634ac3d3d4f4fe1d11a3ea2fac8edf1ea5b
Szymon <s.rydzewski26@gmail.com>
2024-11-02 14:44:26 +0100

    Po wpisaniu przez uzytkownika sciezki program zaczyna sprawdzac sciezke i zapisywac wszystkie informacje o pliku w consoli

--- file-analyzer/file-analyzer.js
+++ file-analyzer/file-analyzer.js
@@ -13,6 +13,29 @@ const emitter = new EventEmitter()
 emitter.on('plikStart', (plik) => console.log(`Rozpoczęto analizę pliku: ${plik}`))
 emitter.on('plikKoniec', (plik) => console.log(`Zakończono analizę pliku: ${plik}`))
 
-rl.question("Podaj ścieżkę do katalogu: ", (sciezka) => {
 
-})
+rl.question("Podaj ścieżkę do katalogu: ", (sciezka) => {
+    fs.readdir(`./${sciezka}`, (err, items) => {
+        if(err){
+            console.error("Błąd")
+            return
+        }
+        items.forEach((item) => {
+            const pelnaSciezka = path.join(`./${sciezka}`, item)
+            fs.stat(pelnaSciezka, (err, stats) => {
+                if(err){
+                    console.error("Błąd")
+                    return
+                }
+                if(stats.isDirectory()){
+                    console.log(`Folder: ${item}`)
+                }
+                else{
+                    console.log(`Plik: ${item}\nRozmiar: ${stats.size}\nRozszerzenie: ${path.extname(item)}\nData ostatniej modyfikacji: ${stats.mtime}\n`)
+                }
+            })
+        })
+    })
+    rl.close()
+    }
+)
